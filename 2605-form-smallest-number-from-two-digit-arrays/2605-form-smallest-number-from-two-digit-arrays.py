668d857ae56fa33efdfe0099f4a665cb4cbf2e66
Sudarshan Madbhavi <148315780+SudarshanSM@users.noreply.github.com>
2026-02-19 17:35:45 +0530

    Time: 0 ms (100%), Space: 19.4 MB (24.12%) - LeetHub

---
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minNumber(self, nums1: List[int], nums2: List[int]) -> int:
+        common = set(nums1) & set(nums2)
+        if common:
+            return min(common)
+
+        min1=min(nums1)
+        min2=min(nums2)
+
+        if min1!=min2:
+            return min(min1 * 10 + min2, min2 * 10 + min1)
+            
+
+        
